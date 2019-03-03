36d5f4f3d18db4c94c1c9a7f6e7eb10b4c889759
csmacdonald95 <csmacdonald95@gmail.com>
2019-03-03 12:00:33 -0400

    Fix missing std::

--- src/Cbb/Midi.cpp
+++ src/Cbb/Midi.cpp
@@ -9,7 +9,8 @@ namespace Midi {
 
 NoteNumber frequencyHzToNoteNumber(const float frequencyHz, const Tuning tuning) noexcept
 {
-    return NoteNumber(A4 + int(roundf(12.0f * std::log2(frequencyHz / tuning.frequencyOfA4Hz))));
+    return NoteNumber(A4
+                      + int(std::roundf(12.0f * std::log2(frequencyHz / tuning.frequencyOfA4Hz))));
 }
 
 float noteNumberToFrequencyHz(const NoteNumber number, const Tuning tuning) noexcept
